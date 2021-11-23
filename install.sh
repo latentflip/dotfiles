bb8ba115cb6f79c2ca8bd57ff6a8af32335abd97
Philip Roberts <latentflip@github.com>
2021-11-23 11:25:52 +0000

    Update install

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir $HOME/bin
-ln -s $(pwd)/git-recent $HOME/bin
+ln -s $(pwd)/bin/git-recent $HOME/bin
 
 ln -s $(pwd)/.aliases.bashrc $HOME/.aliases.bashrc
 ln -s $(pwd)/gitconfig $HOME/.gitconfig
 
 echo source $HOME/.aliases.bashrc >> $HOME/.bashrc
+echo "PATH=$PATH/bin" >> $HOME/.bashrc
