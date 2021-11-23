b54f5c8785cb50c12f97c5857f4ee20c90e2cd0d
Philip Roberts <latentflip@github.com>
2021-11-23 11:32:11 +0000

    Fix paths

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir $HOME/bin
-ln -s $(pwd)/bin/git-recent $HOME/bin
+ln -s $(pwd)/bin $HOME/bin
 
 ln -s $(pwd)/.aliases.bashrc $HOME/.aliases.bashrc
 ln -s $(pwd)/gitconfig $HOME/.gitconfig
 
-echo source $HOME/.aliases.bashrc >> $HOME/.bashrc
-echo "PATH=$PATH/bin" >> $HOME/.bashrc
+echo "source $HOME/.aliases.bashrc" >> $HOME/.bashrc
+echo "PATH=\$PATH:$HOME/bin" >> $HOME/.bashrc
